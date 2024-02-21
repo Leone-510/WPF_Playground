2098af0c9ba296d05035f848fd8bbfbb4e368dd6
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-21 18:16:02 +0200

    Added CommandBase class

---
+++ WPF_Tutorials/NavigationMVVM/Commands/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace NavigationMVVM.Commands
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
