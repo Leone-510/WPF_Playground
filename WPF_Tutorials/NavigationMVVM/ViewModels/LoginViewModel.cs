cb3d3a171d94edf19e3625dc3c6a20baa1af27e2
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-21 20:41:20 +0200

    Added LoginViewModel with LoginCommand

---
+++ WPF_Tutorials/NavigationMVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Stores;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _userName;
+
+        public string Username
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel(NavigationStore navigationStore)
+        {
+            LoginCommand = new NavigateCommand<AccountViewModel>(navigationStore,
+                () => new AccountViewModel(navigationStore));
+        }
+    }
+}
