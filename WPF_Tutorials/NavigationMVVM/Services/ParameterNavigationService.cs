9d7aa9ac9bc57d02a65ef2264063d93b68c2087c
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-22 12:10:12 +0200

    Modified ParameterNavigationService (constructor)

--- WPF_Tutorials/NavigationMVVM/Services/ParameterNavigationService.cs
+++ WPF_Tutorials/NavigationMVVM/Services/ParameterNavigationService.cs
@@ -12,6 +12,12 @@ namespace NavigationMVVM.Services
         private readonly NavigationStore _navigationStore;
         private readonly Func<TParameter, TViewModel> _createViewModel;
 
+        public ParameterNavigationService(NavigationStore navigationStore, Func<TParameter, TViewModel> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
         public void Navigate(TParameter parameter)
         {
             _navigationStore.CurrentViewModel = _createViewModel(parameter);
