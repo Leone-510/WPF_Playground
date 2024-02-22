249c7791c86e812bb40886f46a067be479912de5
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-22 17:41:00 +0200

    Added a LayoutViewModel class

---
+++ WPF_Tutorials/NavigationMVVM/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class LayoutViewModel : ViewModelBase
+    {
+        public NavigationBarViewModel NavigationBarViewModel { get; }
+        public ViewModelBase ContentViewModel { get; }
+
+        public LayoutViewModel(NavigationBarViewModel navigationBarViewModel, ViewModelBase contentViewModel)
+        {
+            NavigationBarViewModel = navigationBarViewModel;
+            ContentViewModel = contentViewModel;
+        }
+
+    }
+}
