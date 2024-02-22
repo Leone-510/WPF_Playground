c3ef8149fa133167e30bfc9620abb78c3e5aa88f
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-22 16:40:39 +0200

    Added NavigationBarViewModel class

---
+++ WPF_Tutorials/NavigationMVVM/ViewModels/NavigationBarViewModel.cs
@@ -0,0 +1,24 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Services;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class NavigationBarViewModel : ViewModelBase
+    {
+        public ICommand NavigateHomeCommand { get; set; }
+        public ICommand NavigateAccountCommand { get; set; }
+        public ICommand NavigateLoginCommand { get; set; }
+
+        public NavigationBarViewModel(
+            NavigationService<HomeViewModel> homeNavigationService,
+            NavigationService<AccountViewModel> accountNavigationService,
+            NavigationService<LoginViewModel> loginNavigationService)
+        {
+            NavigateHomeCommand = new NavigateCommand<HomeViewModel>(homeNavigationService);
+            NavigateAccountCommand = new NavigateCommand<AccountViewModel>(accountNavigationService);
+            NavigateLoginCommand = new NavigateCommand<LoginViewModel>(loginNavigationService);
+        }
+
+    }
+}
