2b5820a82c5ef3f9e53ba96f91a04256d4252dbb
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-22 12:00:38 +0200

    Added ParameterNavigationService class

---
+++ WPF_Tutorials/NavigationMVVM/Services/ParameterNavigationService.cs
@@ -0,0 +1,20 @@
+﻿using NavigationMVVM.Stores;
+using NavigationMVVM.ViewModels;
+
+namespace NavigationMVVM.Services
+{
+    // Note: To support multiple parameters, we create an object to hold
+    // all the parameters and use that object as the TParameter type
+
+    public class ParameterNavigationService<TParameter, TViewModel>
+        where TViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TParameter, TViewModel> _createViewModel;
+
+        public void Navigate(TParameter parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel(parameter);
+        }
+    }
+}
