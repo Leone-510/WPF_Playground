cd648ff70500ad10356762a5268937eb09978b40
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-23 11:42:49 +0200

    Updated NavigationBarViewModel (OnPropertyChanged & Dispose)

--- WPF_Tutorials/NavigationMVVM/ViewModels/NavigationBarViewModel.cs
+++ WPF_Tutorials/NavigationMVVM/ViewModels/NavigationBarViewModel.cs
@@ -8,9 +8,11 @@ namespace NavigationMVVM.ViewModels
     public class NavigationBarViewModel : ViewModelBase
     {
         private readonly AccountStore _accountStore;
-        public ICommand NavigateHomeCommand { get; set; }
-        public ICommand NavigateAccountCommand { get; set; }
-        public ICommand NavigateLoginCommand { get; set; }
+        public ICommand NavigateHomeCommand { get; }
+        public ICommand NavigateAccountCommand { get; }
+        public ICommand NavigateLoginCommand { get; }
+        public ICommand LogoutCommand { get; }
+
         public bool IsLoggedIn => _accountStore.IsLoggedIn;
 
         public NavigationBarViewModel(AccountStore accountStore,
@@ -22,7 +24,21 @@ namespace NavigationMVVM.ViewModels
             NavigateHomeCommand = new NavigateCommand<HomeViewModel>(homeNavigationService);
             NavigateAccountCommand = new NavigateCommand<AccountViewModel>(accountNavigationService);
             NavigateLoginCommand = new NavigateCommand<LoginViewModel>(loginNavigationService);
+            LogoutCommand = new LogoutCommand(_accountStore);
+
+            _accountStore.CurrentAccountChanged += OnCurrentAccountChanged;
         }
 
+        private void OnCurrentAccountChanged()
+        {
+            OnPropertyChanged(nameof(IsLoggedIn));
+        }
+
+        public override void Dispose()
+        {
+            _accountStore.CurrentAccountChanged -= OnCurrentAccountChanged;
+
+            base.Dispose();
+        }
     }
 }
