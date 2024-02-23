3362ed68104ce3dfb5154121a68dfd00ba5673b9
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-23 16:58:05 +0200

    Basic configuration of WPF app to handle Dependency Injection

--- WPF_Tutorials/DIInWPF/App.xaml.cs
+++ WPF_Tutorials/DIInWPF/App.xaml.cs
@@ -1,8 +1,37 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace DIInWPF;
 
 public partial class App : Application
 {
+    public static IHost? AppHost { get; private set; }
 
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupWindow = AppHost.Services.GetRequiredService<MainWindow>();
+        startupWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+
+        base.OnExit(e);
+    }
 }
