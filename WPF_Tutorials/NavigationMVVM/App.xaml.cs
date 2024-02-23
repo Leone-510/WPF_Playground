c7ddce7ed60d483a112d021517483878e544d98d
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-23 12:43:38 +0200

    Updated App.xaml.cs after changing INavigationService to non-generic

--- WPF_Tutorials/NavigationMVVM/App.xaml.cs
+++ WPF_Tutorials/NavigationMVVM/App.xaml.cs
@@ -21,7 +21,7 @@ namespace NavigationMVVM
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            INavigationService<HomeViewModel> homeNavigationService = CreateHomeNavigationService();
+            INavigationService homeNavigationService = CreateHomeNavigationService();
             homeNavigationService.Navigate();
 
             MainWindow = new MainWindow()
@@ -33,7 +33,7 @@ namespace NavigationMVVM
             base.OnStartup(e);
         }
 
-        private INavigationService<HomeViewModel> CreateHomeNavigationService()
+        private INavigationService CreateHomeNavigationService()
         {
             return new LayoutNavigationService<HomeViewModel>(
                 _navigationStore,
@@ -41,14 +41,14 @@ namespace NavigationMVVM
                 CreateNavigationBarViewModel);
         }
 
-        private INavigationService<LoginViewModel> CreateLoginNavigationService()
+        private INavigationService CreateLoginNavigationService()
         {
             return new NavigationService<LoginViewModel>(
                 _navigationStore,
                 () => new LoginViewModel(_accountStore, CreateAccountNavigationService()));
         }
 
-        private INavigationService<AccountViewModel> CreateAccountNavigationService()
+        private INavigationService CreateAccountNavigationService()
         {
             return new LayoutNavigationService<AccountViewModel>(
                 _navigationStore,
