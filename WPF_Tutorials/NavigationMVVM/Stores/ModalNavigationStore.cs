b5ced8502be9073464f1b28236b894d60acf2526
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-23 12:19:53 +0200

    Added ModalNavigationStore class

---
+++ WPF_Tutorials/NavigationMVVM/Stores/ModalNavigationStore.cs
@@ -0,0 +1,34 @@
+﻿using NavigationMVVM.ViewModels;
+
+namespace NavigationMVVM.Stores
+{
+    public class ModalNavigationStore
+    {
+        public bool IsOpen => CurrentViewModel != null; // If CurrentViewModel is null, then modal should not be open
+
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel?.Dispose();
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action CurrentViewModelChanged;
+
+        public void Close()
+        {
+            CurrentViewModel = null;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
