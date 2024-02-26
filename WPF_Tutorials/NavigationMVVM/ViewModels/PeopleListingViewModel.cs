d3175a65dcd3a5e02b588ceb5adf5ddfbc5615f5
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-26 13:40:31 +0200

    Modified PeopleListingViewModel - Added AddPersonCommand

--- WPF_Tutorials/NavigationMVVM/ViewModels/PeopleListingViewModel.cs
+++ WPF_Tutorials/NavigationMVVM/ViewModels/PeopleListingViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Services;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace NavigationMVVM.ViewModels
 {
@@ -8,8 +11,12 @@ namespace NavigationMVVM.ViewModels
 
         public IEnumerable<PersonViewModel> People => _people;
 
-        public PeopleListingViewModel()
+        public ICommand AddPersonCommand { get; }
+
+        public PeopleListingViewModel(INavigationService addPersonNavigationService)
         {
+            AddPersonCommand = new NavigateCommand(addPersonNavigationService);
+
             _people = new ObservableCollection<PersonViewModel>();
 
             _people.Add(new PersonViewModel("George"));
