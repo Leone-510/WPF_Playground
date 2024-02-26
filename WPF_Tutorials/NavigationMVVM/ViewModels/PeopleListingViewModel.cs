0ebe4d1bd82278385a6b22486250318d6fb4e7cc
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-26 14:42:55 +0200

    Modified PeopleListingViewModel - Update collection of People after adding new Person

--- WPF_Tutorials/NavigationMVVM/ViewModels/PeopleListingViewModel.cs
+++ WPF_Tutorials/NavigationMVVM/ViewModels/PeopleListingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using NavigationMVVM.Commands;
 using NavigationMVVM.Services;
+using NavigationMVVM.Stores;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -8,20 +9,36 @@ namespace NavigationMVVM.ViewModels
     public class PeopleListingViewModel : ViewModelBase
     {
         private readonly ObservableCollection<PersonViewModel> _people;
+        private readonly PeopleStore _peopleStore;
 
         public IEnumerable<PersonViewModel> People => _people;
 
         public ICommand AddPersonCommand { get; }
 
-        public PeopleListingViewModel(INavigationService addPersonNavigationService)
+        public PeopleListingViewModel(PeopleStore peopleStore, INavigationService addPersonNavigationService)
         {
+            _peopleStore = peopleStore;
+
             AddPersonCommand = new NavigateCommand(addPersonNavigationService);
 
             _people = new ObservableCollection<PersonViewModel>();
-
             _people.Add(new PersonViewModel("George"));
             _people.Add(new PersonViewModel("Mary"));
             _people.Add(new PersonViewModel("Anna"));
+            
+            _peopleStore.PersonAdded += OnPersonAdded;
+        }
+
+        private void OnPersonAdded(string name)
+        {
+            _people.Add(new PersonViewModel(name));
+        }
+
+        public override void Dispose()
+        {
+            _peopleStore.PersonAdded -= OnPersonAdded;
+
+            base.Dispose();
         }
     }
 }
