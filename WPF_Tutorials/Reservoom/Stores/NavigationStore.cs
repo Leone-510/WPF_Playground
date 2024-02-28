ed719fc1b0566432bd564e4dd92163b7c5cf35c1
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-28 18:35:59 +0200

    Reservoom - Added NavigationStore class

---
+++ WPF_Tutorials/Reservoom/Stores/NavigationStore.cs
@@ -0,0 +1,28 @@
+﻿using Reservoom.ViewModels;
+
+namespace Reservoom.Stores
+{
+    // The setter is also a good place to dispose viewmodels
+    // https://www.youtube.com/watch?v=N26C_Cq-gAY&list=PLA8ZIAm2I03ggP55JbLOrXl6puKw4rEb2&index=1
+
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action CurrentViewModelChanged;
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
