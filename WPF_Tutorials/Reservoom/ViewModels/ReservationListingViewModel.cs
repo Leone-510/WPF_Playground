9634698150d555c6205b25f99a37548383b9126e
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-28 17:26:40 +0200

    Resevroom - Updated ReservationListingViewModel (properties & dummy data)

--- WPF_Tutorials/Reservoom/ViewModels/ReservationListingViewModel.cs
+++ WPF_Tutorials/Reservoom/ViewModels/ReservationListingViewModel.cs
@@ -1,10 +1,46 @@
-﻿namespace Reservoom.ViewModels
+﻿using Reservoom.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Reservoom.ViewModels
 {
     public class ReservationListingViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<ReservationViewModel> _reservations;
+        public IEnumerable<ReservationViewModel> Reservations => _reservations;
+
+
+        public ICommand MakeReservationCommand { get; }
+
         public ReservationListingViewModel()
         {
-            
+            _reservations = new ObservableCollection<ReservationViewModel>();
+
+            #region Adding dummy data
+            _reservations.Add(
+                new ReservationViewModel(
+                    new Reservation(
+                        new RoomID(1, 2),
+                            "Leone",
+                            DateTime.Now,
+                            DateTime.Now.AddDays(5))));
+
+            _reservations.Add(
+                new ReservationViewModel(
+                    new Reservation(
+                        new RoomID(3, 2),
+                            "Joe",
+                            DateTime.Now,
+                            DateTime.Now)));
+
+            _reservations.Add(
+                new ReservationViewModel(
+                    new Reservation(
+                        new RoomID(2, 4),
+                            "May",
+                            DateTime.Now,
+                            DateTime.Now)));
+            #endregion
         }
     }
 }
