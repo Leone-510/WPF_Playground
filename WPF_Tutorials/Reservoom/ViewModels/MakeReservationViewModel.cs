9d5774c3812fbd04f6ffbf0f6b9a2200d95cb050
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-28 17:25:44 +0200

    Resevroom - Updated MakeReservationViewModel (private fields & public properties)

--- WPF_Tutorials/Reservoom/ViewModels/MakeReservationViewModel.cs
+++ WPF_Tutorials/Reservoom/ViewModels/MakeReservationViewModel.cs
@@ -1,10 +1,70 @@
-﻿namespace Reservoom.ViewModels
+﻿using System.Windows.Input;
+
+namespace Reservoom.ViewModels
 {
     public class MakeReservationViewModel : ViewModelBase
     {
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private int _floorNumber;
+        public int FloorNumber
+        {
+            get { return _floorNumber; }
+            set
+            {
+                _floorNumber = value;
+                OnPropertyChanged(nameof(FloorNumber));
+            }
+        }
+
+        private int _roomNumber;
+        public int RoomNumber
+        {
+            get { return _roomNumber; }
+            set
+            {
+                _roomNumber = value;
+                OnPropertyChanged(nameof(RoomNumber));
+            }
+        }
+
+        private DateTime _startDate = new DateTime(year: 2021, month: 1, day: 1);
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged(nameof(StartDate));
+            }
+        }
+
+        private DateTime _endDate = new DateTime(year: 2021, month: 1, day: 8);
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged(nameof(EndDate));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+
         public MakeReservationViewModel()
         {
-            
+
         }
     }
 }
