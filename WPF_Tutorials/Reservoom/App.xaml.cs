e94d12a979c60b0f85ed824dc7e72d9206afdbe8
Leonidas Pigounakis <leonidas.pigounakis@prismael.com>
2024-02-28 15:56:48 +0200

    Resevroom - Modified App.xaml.cs (overrid OnStartup - dummy reservations)

--- WPF_Tutorials/Reservoom/App.xaml.cs
+++ WPF_Tutorials/Reservoom/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,32 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Leone Suites Hotel");
 
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                        new RoomID(1, 3),
+                        "Leone",
+                        new DateTime(2015, 1, 1),
+                        new DateTime(2015, 1, 2)));
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 2),
+                    "Leone",
+                    new DateTime(2015, 1, 3),
+                    new DateTime(2015, 1, 4)));
+            }
+            catch (ReservationConflictException ex)
+            {
+                string exMessage = ex.Message;
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("Leone");
+
+            base.OnStartup(e);
+        }
+    }
 }
